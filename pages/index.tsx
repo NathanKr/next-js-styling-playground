2757e24b7a5adf29754836c3255d6d4bfe3df78a
Nathan Krasney <natankrasney@gmail.com>
2022-08-17 11:28:52 +0300

    initial version

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Hello Next.js</h2>
+    </div>
+  )
+}
+
+export default Home
